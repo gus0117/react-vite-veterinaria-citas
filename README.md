b6eefa481d4712b5a36eac011ff0b3668017f31f
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2022-12-11 12:55:26 -0300

    Primer commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Proyecto Citas-Veterinaria con React-Vite
---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import Formulario from "./components/Formulario";
+import Header from "./components/Header";
+import ListadoPacientes from "./components/ListadoPacientes";
+
+function App() {
+  const [pacientes, setPacientes] = useState([]);
+  const [paciente, setPaciente] = useState({})
+
+  const eliminarPaciente = (id) => {
+    const pacientesActualizado = pacientes.filter(pacienteState => pacienteState.id !== id);
+    setPacientes(pacientesActualizado);
+  }
+
+  //
+  useEffect(()=>{
+    const obtenerLocalStorage = () => {
+      const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+      setPacientes(pacientesLS);
+    }
+
+    obtenerLocalStorage();
+  },[])
+
+  //Sincroniza los pacientes en local storage
+  useEffect(()=>{
+    localStorage.setItem('pacientes', JSON.stringify( pacientes ));
+  },[pacientes])
+
+  return (
+    <div className="container mx-auto mt-20">
+      <Header />
+      <div className="mt-12 md:flex">
+        <Formulario 
+          pacientes={pacientes}
+          setPacientes={setPacientes}
+          paciente={paciente}
+          setPaciente={setPaciente}
+        />
+        <ListadoPacientes 
+          pacientes={pacientes}
+          setPaciente={setPaciente}
+          eliminarPaciente={eliminarPaciente}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default App
